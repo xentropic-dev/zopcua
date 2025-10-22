2d91a7620823989191b1d1c44cb7f5112694f837
Chris Laponsie <claponsie@gmail.com>
2025-10-22 17:48:48 -0400

    Removed unnecessary files

--- test_exe.zig
+++
@@ -1,9 +0,0 @@
-const std = @import("std");
-const ua = @import("src/root.zig");
-
-pub fn main() !void {
-    std.debug.print("About to call Server.init()\n", .{});
-    var server = try ua.Server.init();
-    defer server.deinit();
-    std.debug.print("Server.init() succeeded\n", .{});
-}
